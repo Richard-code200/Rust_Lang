ae7abbe46dcb1d0e0d83dc721d32a43740dec934
Richard-code200 <ERRORichard@protonmail.com>
2026-08-26 23:40:00 +0800

    rust-vec

---
+++ collections_learn/src/main.rs
@@ -0,0 +1,45 @@
+#![allow(unused)]
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    for num in &v {
+        println!("the number is {num}");
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+    match v.get(2) {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element,"),
+    }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("Hello, world!");
+}
