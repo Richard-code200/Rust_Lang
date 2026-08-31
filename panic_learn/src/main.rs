606ed2d482d5ce44e3b3e7e8ec1a5a2e0aaf63f9
Richard-code200 <ERRORichard@protonmail.com>
2026-08-31 23:41:19 +0800

    panic

--- panic_learn/src/main.rs
+++ panic_learn/src/main.rs
@@ -20,6 +20,15 @@ fn main() {
             other_error => panic!("Problem opening the file: {other_error:?}"),
         },
     };
+    println!("part two \n");
 
-    println!("Hello, world!");
+    let greeting_file_result = File::open("hello.txt").unwrap();
+    //unwrap()用于提取Option或Result类型内部的值,如果是None或Err,程序将panic并终止
+    println!("{greeting_file_result:?}");
+
+    let greeting_file_result = File::open("hello.txt").expect("File not found");
+    println!("{greeting_file_result:?}");
+    //expect()与unwrap()类似,但它允许自定义错误消息
+
+    println!("\nHello, world!");
 }
